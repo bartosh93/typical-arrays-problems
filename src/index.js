f9eb7c476867ca2bdc859b509cb9fdeaa6f11fef
Uladzimir_Bartash <bartosh93@yandex.ru>
2020-10-03 01:01:19 +0300

    feat: finish typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 
 exports.min = function min (array) {
-  return 0;
+  let maxi;
+  
+  if ( array === undefined || array.length == 0  ) {
+    maxi = 0;
+  }  
+  else {
+    maxi = array[0];  
+    for(i=0; i < array.length -1; i++){
+        if (maxi < array[i+1] ) {
+          maxi = maxi;
+        }
+        else {
+            maxi = array[i+1];
+        }
+        
+        }  
+      }
+
+  return maxi;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxi;
+  
+  if ( array === undefined || array.length == 0  ) {
+    maxi = 0;
+  }  
+  else {
+    maxi = array[0];  
+    for(i=0; i < array.length -1; i++){
+        if (maxi > array[i+1] ) {
+          maxi = maxi;
+        }
+        else {
+            maxi = array[i+1];
+        }
+        
+        }  
+      }
+
+  return maxi;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  let resul = 0;
+    if ( array === undefined || array.length == 0  ) {
+      result = 0;}
+      else {
+        
+        for(i=0; i < array.length; i++){
+          resul += array[i];
+        }
+        result = resul / array.length;
+      }
+        
+      
+      return result;
+    
+
+      
+      
 }
+  
